fab1593d6f470728ec7ac95ecbc00be308b01f6d
Vivek Dalal <dalal.vi@husky.neu.edu>
2020-05-22 00:01:12 -0400

    Code Deploy + Build + Pipeline

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -6,9 +6,16 @@ CLI_PROFILE=admin
 
 EC2_INSTANCE_TYPE=t2.micro
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile admin \
   --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+
+# Generate a personal access token with repo and admin:repo_hook
+#    permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat .github/aws-bootstrap-access-token)
+GH_OWNER=$(cat .github/aws-bootstrap-owner)
+GH_REPO=$(cat .github/aws-bootstrap-repo)
+GH_BRANCH=master
 
 # Deploys static resources
 echo -e "\n\n=========== Deploying setup.yml ==========="
@@ -22,14 +29,19 @@ aws cloudformation deploy \
   --parameter-overrides \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-#Deploy the CloudFormation template
-echo -e "\n\n==============Deploying main.yml============"
+
+echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
-    --region $REGION \
-    --profile $CLI_PROFILE \
-    --stack-name $STACK_NAME \
-    --template-file main.yml \
-    --no-fail-on-empty-changeset \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides \
-      EC2InstanceType=$EC2_INSTANCE_TYPE
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
